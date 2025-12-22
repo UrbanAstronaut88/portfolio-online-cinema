4fa0c60f2a2b0cbed52a516ca444e225fef4964b
BondarenkoKirill <bku089@gmail.com>
2025-12-22 20:21:32 +0200

    chore: configure async database session and dependency

--- app/db/session.py
+++ app/db/session.py
@@ -1,16 +1,20 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-import os
+
+from app.core.config import settings
 
 
-# for develop SQLite with aiosqlite; for prod - postgresql+asyncpg://... from env !
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./test.db")
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=False
+)
 
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
 
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
-
-AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 async def get_db():
     async with AsyncSessionLocal() as db:
