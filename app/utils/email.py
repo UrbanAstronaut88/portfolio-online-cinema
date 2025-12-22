a78da52c8ef84ec3d604287b4305fc0ed0175fca
BondarenkoKirill <bku089@gmail.com>
2025-12-22 20:13:45 +0200

    feat(email): add email sending utility with SMTP configuration

--- app/utils/email.py
+++ app/utils/email.py
@@ -13,7 +13,7 @@ class EmailSettings(BaseSettings):
     MAIL_FROM: str = os.getenv("MAIL_FROM", "urbanastronaut88@gmail.com")
     MAIL_PORT: int = int(os.getenv("MAIL_PORT", 587))
     MAIL_SERVER: str = os.getenv("MAIL_SERVER", "smtp.gmail.com")
-    MAIL_FROM_NAME: str = os.getenv("MAIL_FROM_NAME", "Online Cinema")
+    MAIL_FROM_NAME: str = os.getenv("MAIL_FROM_NAME", "Online Cinema Portfolio Project")
 
 conf = ConnectionConfig(
     MAIL_USERNAME=EmailSettings().MAIL_USERNAME,
@@ -28,12 +28,13 @@ conf = ConnectionConfig(
     VALIDATE_CERTS=True
 )
 
+
 async def send_email(subject: str, recipients: list, body: str):
     message = MessageSchema(
         subject=subject,
         recipients=recipients,
         body=body,
-        subtype="html"  # for HTML
+        subtype="html"
     )
     fm = FastMail(conf)
     await fm.send_message(message)
